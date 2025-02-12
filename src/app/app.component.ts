5176172a2ae985eb96ac44f89f0ffeb582c694eb
Ray <rainer.musch@gmx.de>
2025-02-12 10:21:08 +0100

    add fruitlist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FruitlistComponent} from './fruitlist/fruitlist.component'
+
 
 @Component({
   selector: 'app-root',
---
+++ src/app/fruitlist/fruitlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruitlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './fruitlist.component.html',
+  styleUrl: './fruitlist.component.scss'
+})
+export class FruitlistComponent {
+
+}
